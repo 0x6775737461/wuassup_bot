55a5b186cc1d44c013d1e1f08f26c3e7a8e49a84
0x6775737461 <fabiogustavo@tuta.io>
2022-02-04 12:08:11 -0400

    logging feature

--- src/py/bot.py
+++ src/py/bot.py
@@ -3,6 +3,8 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 
+import logging
+
 load_dotenv()
 
 account_sid = os.getenv("TWILIO_SID")
@@ -12,10 +14,13 @@ my_num = os.getenv("MY_NUM")
 
 client = Client(account_sid, auth_token) 
 
+logging.basicConfig()
+client.http_client.logger.setLevel(logging.INFO)
+
 message = client.messages.create( 
     from_='whatsapp:' + twilio_num,  
     body='TESTANDO O TWILLIO CARAIO!!',      
     to='whatsapp:' + my_num 
-) 
+)
 
 print(message.sid)
